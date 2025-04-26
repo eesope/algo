a38c61f05e348b234ba6d90c6ac33159088ea0d8
Saeyoung Park <spark363@my.bcit.ca>
2025-04-26 11:30:41 -0700

    Add better sol

--- algorithm_questions/two_sum.ex
+++ algorithm_questions/two_sum.ex
@@ -23,3 +23,28 @@ defmodule Solution do
         # end)
     end
 end
+
+
+# better solution; the pattern matching
+
+defmodule Solution do
+  @spec two_sum(nums :: [integer], target :: integer) :: [integer]
+
+  def two_sum(nums, target), do: two_sum(nums, target, %{}, 0)
+
+  def two_sum([], _target, _acc, _index), do: []
+
+  def two_sum([num | rest], target, acc, index) do
+    complement = target - num
+
+    case Map.get(acc, complement) do
+      nil ->
+        acc = Map.put(acc, num, index)
+        index = index + 1
+        two_sum(rest, target, acc, index)
+
+      complement_index ->
+        [complement_index, index]
+    end
+  end
+end
